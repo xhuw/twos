de779ec9d484d7dfe2d0c85ecb2b24ba82a81199
Huw Percival <huwpercival@xmos.com>
2023-11-02 16:13:57 +0000

    next

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="ts">
+import { tick } from 'svelte';
+import { fade, fly } from 'svelte/transition';
+
+let text_input;
+let show_res = false;
+let res = 0
+
+function bin_to_num(b) {
+    if(!b.length) {
+        return 0;
+    }
+    let accum = -1 * (Math.pow(2, (b.length - 1))) * parseInt(b[0]);
+    for(let i = 1; i < b.length; i++) {
+        accum += Math.pow(2, b.length - (i + 1)) * parseInt(b[i]);
+    }
+    return accum;
+}
+
+async function update_res() {
+    show_res = false;
+    await tick();
+    res = bin_to_num(text_input);
+    show_res = true;
+}
+
+</script>
+
+
+<h1>Converting twos complement for people in need</h1>
+<input bind:value={text_input} placeholder="101011"/>
+<button on:click={update_res}>Convert</button>
+{#if show_res}
+<p in:fly={{ y: 200, duration: 2000 }} out:fade>{res}</p>
+{/if}
